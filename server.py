82c98ed52763f37ede798d7624e8da167d5f5ec0
Darkcloud1224 <127832687+Darkcloud1224@users.noreply.github.com>
2024-07-11 17:22:09 +0800

    Update server.py

--- server.py
+++ server.py
@@ -1,19 +1,35 @@
-import os
-import http.server
-import socketserver
+import asyncio
+import websockets
 
-from http import HTTPStatus
+# Dictionary to store connected clients in their respective rooms
+rooms = {}
 
+async def handle_client(websocket, path):
+    # Get the room ID from the client
+    room_id = await websocket.recv()
+    
+    # Create the room if it doesn't exist
+    if room_id not in rooms:
+        rooms[room_id] = set()
 
-class Handler(http.server.SimpleHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(HTTPStatus.OK)
-        self.end_headers()
-        msg = 'Hello! you requested %s' % (self.path)
-        self.wfile.write(msg.encode())
+    # Add the client to the room
+    rooms[room_id].add(websocket)
+    print(f"Client joined room {room_id}")
 
+    try:
+        async for message in websocket:
+            # Broadcast the message to all clients in the room
+            for client in rooms[room_id]:
+                if client != websocket:  # Don't send the message back to the sender
+                    await client.send(message)
+                    print(f"Message broadcasted in room {room_id}: {message}")
 
-port = int(os.getenv('PORT', 80))
-print('Listening on port %s' % (port))
-httpd = socketserver.TCPServer(('', port), Handler)
-httpd.serve_forever()
+    finally:
+        # Remove the client from the room upon disconnection
+        rooms[room_id].remove(websocket)
+        print(f"Client left room {room_id}")
+
+start_server = websockets.serve(handle_client, "localhost", 8765)
+
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()
